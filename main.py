b1c1a73faadcf27e0e362d5b15de50ac31fe754a
musicakc <amitoj1606@gmail.com>
2015-05-22 20:52:24 +0530

    Adding some documentation

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ TWITTER_ACCESS_TOKEN_SECRET= ''
 
 t = Twitter(auth=OAuth(TWITTER_ACCESS_TOKEN,TWITTER_ACCESS_TOKEN_SECRET,TWITTER_APP_KEY,TWITTER_APP_KEY_SECRET))
 
+
+# Function to search a particular term in tweets
 def search_tweets(q,count=10,m_id=None):
     return t.search.tweets(q=q,result_type='recent',count=count,lang="en",max_id=m_id)
 
+# Function to favorite a tweet
 def favorites_create(tweet):
     try:
         result = t.favorites.create(_id=tweet['id'])
@@ -19,6 +22,7 @@ def favorites_create(tweet):
         print "Error: ", e
         return None
 
+# Function to search and favorite the tweet with the required term 
 def search_and_fav(q,count=10,m_id=None):
     result = search_tweets(q,count,m_id)
     first_id=result['statuses'][0]['id']
@@ -31,7 +35,6 @@ def search_and_fav(q,count=10,m_id=None):
     print "Favorites total: %i of %i" % (success, len(result['statuses']))
     print "First id %s last id %s" %(first_id, last_id)
 
-search_and_fav('natural language processing',3)    
 
-    
-    
+# As an example searching for the term "natural language processing"
+search_and_fav('natural language processing',3)
